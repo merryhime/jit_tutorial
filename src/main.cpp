26b6d8e1907ac722fcc2c01075108591a55ca536
Merry <git@mary.rs>
2022-12-29 10:45:49 +0000

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: Copyright (c) 2022 merryhime <https://mary.rs>
+// SPDX-License-Identifier: MIT
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("sanity check", "")
+{
+    REQUIRE(true);
+}
